eb3f1c296016acb6a5e6adfcdc94ce7bb1c79656
Doidae <andyarredondo1224@gmail.com>
2024-06-10 16:27:10 -0400

    Added routes to backend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+
+const User = require('./models/userSchema')
 
 const app = express();
 
@@ -25,5 +28,29 @@ app.use(cors());
 
 
 
-
 //Routes???
+
+//POST
+app.post('/register', async (req, res) => {
+    try {
+        const { email, username, password } = req.body
+        //Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new User({email, username, password: hashedPassword })
+        await newUser.save()
+        res.status(201).json({message: 'New user created successfully'})
+    } catch (error) {
+        res.status(500).json({error: 'Error signing up'})
+    }
+})
+
+//GET
+app.get('/register', async (req,res) => {
+    try {
+        const users = await User.find()
+        res.status(201).json(users)
+    } catch(error) {
+        res.status(500).json({error: 'Could not get Users'})
+    }
+})
+
