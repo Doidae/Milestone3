e692b3ca244a4c98f0e4ea99a2f5ef4c397fc4c6
Doidae <andyarredondo1224@gmail.com>
2024-06-11 20:49:41 -0400

    Created Login Page and styled

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import './style/Login.css'
 
-function Login() {
-    return(
-        <div>
-            Login page
+
+const Login = ({ onLoginSuccess }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5000/login', { username, password });
+            onLoginSuccess(response.data.token); 
+            setUsername('');
+            setPassword('');
+            setError('');
+        } catch (error) {
+            setError('Invalid login credentials');
+        }
+    };
+
+    return (
+        <div className="login-form-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                {error && <p className="login-form__error">{error}</p>}
+                <input
+                    className="login-form__input"
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+                <input
+                    className="login-form__input"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button className="login-form__button" type="submit">Login</button>
+            </form>
         </div>
-    )
-}
+    );
+};
 
 export default Login;
